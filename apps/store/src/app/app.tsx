62dd8bf5a082103eb093a115fb7de4909ac7853a
bendera <szoftverhiba@gmail.com>
2022-01-28 16:47:53 +0100

    08: create react feature libraries
    npx nx g @nrwl/react:lib feature-game-detail --directory=store --appProject=store

--- apps/store/src/app/app.tsx
+++ apps/store/src/app/app.tsx
@@ -13,6 +13,10 @@ import Typography from '@material-ui/core/Typography';
 
 import { formatRating } from '@nxdemo/store/util-formatters';
 
+import { Route, Link } from 'react-router-dom';
+
+import { StoreFeatureGameDetail } from '@nxdemo/store/feature-game-detail';
+
 export const App = () => {
   return (
     <>
@@ -52,6 +56,47 @@ export const App = () => {
           ))}
         </div>
       </div>
+
+      {/* START: routes */}
+      {/* These routes and navigation have been generated for you */}
+      {/* Feel free to move and update them to fit your needs */}
+      <br />
+      <hr />
+      <br />
+      <div role="navigation">
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/feature-game-detail">StoreFeatureGameDetail</Link>
+          </li>
+          <li>
+            <Link to="/page-2">Page 2</Link>
+          </li>
+        </ul>
+      </div>
+      <Route
+        path="/"
+        exact
+        render={() => (
+          <div>
+            This is the generated root route.{' '}
+            <Link to="/page-2">Click here for page 2.</Link>
+          </div>
+        )}
+      />
+      <Route path="/feature-game-detail" component={StoreFeatureGameDetail} />
+      <Route
+        path="/page-2"
+        exact
+        render={() => (
+          <div>
+            <Link to="/">Click here to go back to root page.</Link>
+          </div>
+        )}
+      />
+      {/* END: routes */}
     </>
   );
 };
